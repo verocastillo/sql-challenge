0921a4ce6884bed2758c459baa32a8bc106fd6fe
Vero Castillo <77795761+verocastillo@users.noreply.github.com>
2021-05-21 17:56:20 -0500

    Finished queries. Code updated.

--- EmployeeSQL/sql/queries.sql
+++ EmployeeSQL/sql/queries.sql
@@ -31,14 +31,40 @@ INNER JOIN employees ON employees.emp_no = dpt_managers.emp_no;
 
 -- 4. List the department of each employee with employee number, last name, 
 --    first name, and department name.
+SELECT dpt_employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dpt_employees
+LEFT JOIN employees ON employees.emp_no = dpt_employees.emp_no
+INNER JOIN departments ON dpt_employees.dept_no = departments.dept_no;
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" 
 --    and last names begin with "B."
-
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name IN
+(
+	SELECT first_name
+    FROM employees
+    WHERE first_name = 'Hercules');
+	
 -- 6. List all employees in the Sales department, including their employee number, last 
 --    name, first name, and department name.
+SELECT employees.emp_no, employees.first_name, employees.last_name, departments.dept_name
+FROM employees
+INNER JOIN dpt_employees ON dpt_employees.emp_no = employees.emp_no
+LEFT JOIN departments ON departments.dept_no = dpt_employees.dept_no
+WHERE dept_name = 'Sales';
 
 -- 7. List all employees in the Sales and Development departments, including their 
 --    employee number, last name, first name, and department name.
+SELECT employees.emp_no, employees.first_name, employees.last_name, departments.dept_name
+FROM employees
+INNER JOIN dpt_employees ON dpt_employees.emp_no = employees.emp_no
+LEFT JOIN departments ON departments.dept_no = dpt_employees.dept_no
+WHERE dept_name = 'Sales' OR dept_name = 'Development';
 
 -- 8. In descending order, list the frequency count of employee last names.
+SELECT last_name AS "Last Name", 
+COUNT(last_name) AS "Frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY "Frequency" DESC
